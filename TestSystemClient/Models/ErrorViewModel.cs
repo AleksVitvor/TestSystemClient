e0370c1e910b4c5ee33aab22f799481514a1608c
Aleks <vitvor01@gmail.com>
2021-04-26 22:10:04 +0300

    main: Add project

---
+++ TestSystemClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestSystemClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
