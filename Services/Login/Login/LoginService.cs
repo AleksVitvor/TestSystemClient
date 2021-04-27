d6c7dbd0c79f1ce3dba2f7a94aeb6a9c68662469
Aleks <52856443+AleksVitvor@users.noreply.github.com>
2021-04-27 20:14:03 +0300

    feature3: Add logIn functionalty

---
+++ Services/Login/Login/LoginService.cs
@@ -0,0 +1,40 @@
+﻿using DtoModels.Login;
+using DtoModels.RequestModels.Login;
+using Services.CryptoService;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Services.Login.Login
+{
+    public class LoginService : ILoginService
+    {
+        IHttpClientFactory HttpClientFactory;
+        ICrypto Crypto;
+
+        public LoginService(IHttpClientFactory httpClientFactory, ICrypto crypto)
+        {
+            HttpClientFactory = httpClientFactory;
+            Crypto = crypto;
+        }
+        public async Task<RequestLoginModel> LogIn(LoginModelDto loginModel)
+        {
+            RequestLoginModel requestLogin;
+
+            try
+            {
+                var client = HttpClientFactory.CreateClient();
+                loginModel.Password = Crypto.SHA256GetHash(loginModel.Password);
+                var result = await client.PostAsJsonAsync("http://localhost:3000/user/login", loginModel);
+                requestLogin = await result.Content.ReadFromJsonAsync<RequestLoginModel>();
+            }
+            catch (Exception)
+            {
+                requestLogin = null;
+            }
+
+            return requestLogin;
+        }
+    }
+}
