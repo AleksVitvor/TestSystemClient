7579fafa0df09590eabf3d080345c4bdb0c25764
Aleks <52856443+AleksVitvor@users.noreply.github.com>
2021-05-11 20:43:43 +0300

    feature8: Add work with answers
    * feature8: Add work with answers
    
    * feature8: Fix yaml
    
    * feature8: Fix yaml 2
    
    * feature8: Fix yaml 3
    
    * feature8: Add nuget config
    
    * feature8: Fix dependencies
    
    * feature8: Fix yaml 4
    
    * feature8: Fix yaml 5
    
    * Feature8: Fix yaml
    
    * feature8: Update VS config
    
    * Fix yaml
    
    * Fix yaml
    
    * Fix publish

---
+++ DtoModels/Answers/AnswerDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DtoModels.Answers
+{
+    public class AnswerDto
+    {
+        [ScaffoldColumn(false)]
+        public int AnswerId { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int QuestionId { get; set; }
+
+        public string Answer { get; set; }
+
+        public bool IsRight { get; set; }
+    }
+}
---
+++ Services/Answer/IAnswerService.cs
@@ -0,0 +1,17 @@
+﻿using DtoModels.Answers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Services.Answer
+{
+    public interface IAnswerService
+    {
+        public Task<IList<AnswerDto>> GetAnswers(int questionId, string token);
+
+        public Task<AnswerDto> UpdateAnswer(AnswerDto answer, string token);
+
+        public Task<AnswerDto> CreateAnswer(AnswerDto answer, string token);
+
+        public Task<bool> DeleteAnswer(AnswerDto answer, string token);
+    }
+}
