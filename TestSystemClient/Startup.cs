d421298fbfea61442d193bf7afdff1f632af33aa
Aleks <vitvor01@gmail.com>
2021-05-11 19:36:06 +0300

    feature8: Fix dependencies

--- TestSystemClient/Startup.cs
+++ TestSystemClient/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json.Serialization;
 using Services.Answer;
 using Services.CryptoService;
 using Services.Login.Login;
