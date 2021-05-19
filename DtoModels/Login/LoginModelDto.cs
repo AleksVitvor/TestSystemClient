beafc2c4f83113104b95a85e49cac6b0a721b6a5
Alexey Vitvor <vitvor01@gmail.com>
2021-05-19 09:14:31 +0300

    stage: Add validation on registration and login

--- DtoModels/Login/LoginModelDto.cs
+++ DtoModels/Login/LoginModelDto.cs
@@ -1,9 +1,14 @@
-﻿namespace DtoModels.Login
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DtoModels.Login
 {
     public class LoginModelDto
     {
+        [Required]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Password should be between 4 and 50 characters")]
         public string Password { get; set; }
 
+        [Required]
         public string Username { get; set; }
     }
 }
--- DtoModels/Login/RegisterModelDto.cs
+++ DtoModels/Login/RegisterModelDto.cs
@@ -1,11 +1,17 @@
-﻿namespace DtoModels.Login
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DtoModels.Login
 {
     public class RegisterModelDto
     {
+        [Required]
         public string Fullname { get; set; }
 
+        [Required]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Password should be between 4 and 50 characters")]
         public string Password { get; set; }
 
+        [Required]
         public string Username { get; set; }
     }
 }
