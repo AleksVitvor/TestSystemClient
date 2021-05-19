ef3db6d4cefe334e66f705f466d916824072de72
Alexey Vitvor <vitvor01@gmail.com>
2021-05-19 09:52:11 +0300

    stage: Add redirect to LogIn

--- TestSystemClient/Startup.cs
+++ TestSystemClient/Startup.cs
@@ -84,7 +84,7 @@ namespace TestSystemClient
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Login/Login");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
